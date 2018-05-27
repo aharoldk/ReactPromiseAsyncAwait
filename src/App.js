b7eb2293bbafe607da4510228e1ade9c7ec7253d
Alwandy Harold Kalangi <aharoldk@gmail.com>
2018-05-27 11:23:10 +0800

    Remove async and await

--- src/App.js
+++ src/App.js
@@ -13,24 +13,43 @@ export default class App extends Component {
     }
   }
 
-  async componentDidMount(){
+  
+
+  componentDidMount(){
     const idCity = '1622786'; //makassar
     const cntDay = '16'
     const appId = 'b4951299cd6f2e36520517762fc87bf5'; //this is my appId. Please get a new appId in openweather.org
 
     const url = `https://api.openweathermap.org/data/2.5/forecast/daily?id=${idCity}&cnt=${cntDay}&appid=${appId}`;
-
-    const res = await axios.get(url);
-    const arrayList = await res.data.list;
+    
     const list = this.state.list;
 
-    arrayList.map(lists => {
-      return list.push(lists);
-    })
-    
-    this.setState({list});
+    const thisPromise = data => {
+      return new Promise((resolve, reject) => {
+        if(data !== null){
+          return resolve(data.data.list);
+        } else {
+          return reject(new Error("List Data Null"));
+        }
+      });
+    }
 
-    console.log(this.state.list);
+    axios.get(url)
+    .then(response => {
+      return thisPromise(response)
+    })
+    .then(data => {
+      data.map(lists => {
+        return list.push(lists);
+      })
+
+      this.setState({
+        list
+      });
+
+      console.log(this.state.list);
+    })
+    .catch(e => console.log(e));
   }
 
 
